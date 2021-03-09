d7b72d2b684ab414f6cbf814208ac766cf439c5e
Ahmed BAALWY <ahmadbaalwy@gmail.com>
2021-03-09 22:22:04 +0300

    first version

--- src/app/services/chance-answer.service.ts
+++ src/app/services/chance-answer.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-export const API_URL = 'http://192.168.1.200:8080/api/';
+export const API_URL = 'http://wajeb-project.el.r.appspot.com/api/';
 const headers = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
